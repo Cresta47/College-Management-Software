cba00768d0f5d0faa6c4eff2a3e6e45379985264
Adhikari <sadhikari@icct.com>
2016-08-14 12:40:25 -0400

    sun

---
+++ public/resources/js/routes.js
@@ -0,0 +1,3 @@
+/**
+ * Created by sadhikari on 8/14/2016.
+ */
--- resources/views/partials/user-form.blade.php
+++ resources/views/partials/user-form.blade.php
@@ -1,4 +1,4 @@
-<div class="mdl-grid demo-content" ng-controller="SignupController">
+<div class="mdl-grid demo-content" ng-controller="UserFormController">
     <div class="demo-charts mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid">
         <form>
             <div class="form-group label-static is-empty">
@@ -38,26 +38,45 @@
     </div>
 </div>
 
+<div class="mdl-grid demo-content" ng-controller="UserListCardsController">
+    <div class="demo-charts mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid">
+        <div ng-repeat="user in userCards"
+             ng-show="showCards"
+             class="demo-updates mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--3-col-desktop">
+            <div class="mdl-card__title mdl-card--expand mdl-color--teal-300">
+                <h2 class="mdl-card__title-text">@{{ user.name }}</h2>
+            </div>
+            <div class="mdl-card__supporting-text mdl-color-text--grey-600">
+                @{{ user.email }}
+            </div>
+            <div class="mdl-card__actions mdl-card--border">
+                <a href="#" class="mdl-button mdl-js-button mdl-js-ripple-effect">Profile</a>
+            </div>
+        </div>
+    </div>
 
 
 
+    <table ng-show="showList" class="mdl-data-table mdl-js-data-table mdl-data-table--selectable mdl-shadow--2dp">
+        <thead>
+        <tr>
+            <th class="full-width">ID</th>
+            <th class="mdl-data-table__cell--non-numeric">Name</th>
+            <th>Email</th>
+            <th>Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr ng-repeat="user in userList">
+            <td>@{{ user.id }}</td>
+            <td class="mdl-data-table__cell--non-numeric">John Doe</td>
+            <td>@{{ user.email }}</td>
+            <td>
+                <a href="#" ng-click="showEditForm(user.id)"><i class="fa fa-pencil fa-fw"></i></a>
+            </td>
+        </tr>
+        </tbody>
+    </table>
 
-{{--<div class="mdl-grid demo-content" ng-controller="UserCardsController">--}}
-    {{--<div class="demo-charts mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid">--}}
+</div>
 
-
-            {{--<div ng-repeat="user in users" class="demo-updates mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--3-col-desktop">--}}
-                {{--<div class="mdl-card__title mdl-card--expand mdl-color--teal-300">--}}
-                    {{--<h2 class="mdl-card__title-text">@{{ user.name }}</h2>--}}
-                {{--</div>--}}
-                {{--<div class="mdl-card__supporting-text mdl-color-text--grey-600">--}}
-                    {{--@{{ user.email }}--}}
-                {{--</div>--}}
-                {{--<div class="mdl-card__actions mdl-card--border">--}}
-                    {{--<a href="#" class="mdl-button mdl-js-button mdl-js-ripple-effect">Profile</a>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-
-
-    {{--</div>--}}
-{{--</div>--}}
